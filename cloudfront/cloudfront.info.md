5caffaed4c34f96fec270b53f6ae5be13b2d3483
liemdev <buithanhliem5073@gmail.com>
2026-01-10 16:55:24 +0700

    50% cloudfront

---
+++ cloudfront/cloudfront.info.md
@@ -0,0 +1,43 @@
+# Cloudfront - CDN (Content Delivery Network) - Cache ở các Edge location
+
+    - Caching for S3
+
+    - Caching for Application load balancer
+
+# Distribution
+
+    - Origin: s3, database, ALB,...
+
+    - Edge location: nơi lưu trữ cache
+
+# Logging, Reporting
+
+# Security
+
+    - HTTPS
+
+    - Certificate
+
+    _ ...
+
+# Customize at the Edge
+
+    - Thực thi trên các Edge location
+
+# Use Case
+
+    - Cache images, videos, documents
+
+    - Streaming video
+
+    - Response maintain
+
+# Behavior
+
+    - /api*
+
+    - /page*
+
+    - ...
+
+# Cloudfront - Certificate (required certificate domain in Virginia)
---
+++ cloudfront/lab_1.sh
@@ -0,0 +1,25 @@
+# Create s3 bucket static web devandbug.info.vn
+
+    - Not public access
+
+    - Not policy
+
+# Create distribution CloudFront for the bucket
+
+# Access the website via CloudFront domain name, test performance…
+
+# Register domain devandbug.info.vn on Route53
+
+# Create hosted zone devandbug.info.vn
+
+# Request SSL certificate for domain devandbug.info.vn  (Include www.devandbug.info.vn)
+
+# Configure CloudFront to use domain devandbug.info.vn, www.devandbug.info.vn  (Alternate domain names)
+
+# Set up DNS routing
+
+    - Create A record set for devandbug.info.vn to point to CloudFront distribution
+
+    - Create CNAME www.devandbug.info.vn to point to CloudFront distribution
+
+
