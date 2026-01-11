2b7f8ad935392f14a7563c10caeeda22ca58f1f0
LiemDev <buithanhliem5073@gmail.com>
2026-01-11 13:52:48 +0700

    done route 53

---
+++ route53/lab_1.sh
@@ -0,0 +1,9 @@
+# Create 1 instance EC2 (Amazon Linux 2)
+
+# Create hosted zone for domain devandbug.info.vn
+
+# Create A record set for devandbug.info.vn to point to EC2 instance
+
+# Check DNS propagation using nslookup or dig commands
+
+# Access the website via http://test.devandbug.info.vn
